da210ac4ddcd3df6b98894fddda09b44ad73c15c
Til Mozes <566541@student.fontys.nl>
2025-09-15 13:10:39 +0200

    initial repostory

---
+++ friends-app/src/datasources/db.datasource.ts
@@ -0,0 +1,26 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'test',
+  connector: 'postgresql',
+  url: 'postgresql://test_0zfh_user:nJ2vXSpfRp4OXEgrUyQ4uSmCrWxCWhx7@dpg-d33das3uibrs73ahcfgg-a.frankfurt-postgres.render.com:5432/test_0zfh?ssl=true',
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'db';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.db', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ friends-app/src/models/address.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Friend} from './friend.model';
+
+@model({settings: {strict: false}})
+export class Address extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  city: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  postcode: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  street: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  house_number: string;
+
+  @belongsTo(() => Friend)
+  friendId: number;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Address>) {
+    super(data);
+  }
+}
+
+export interface AddressRelations {
+  // describe navigational properties here
+}
+
+export type AddressWithRelations = Address & AddressRelations;
